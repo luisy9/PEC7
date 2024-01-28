7774010af9e04dd7b05c387b529aa1a6392abcb2
Luis de Haro <luisdeharo4@gmail.com>
2024-01-28 12:11:05 +0100

    guard para saber si estamos logueados o no

--- PEC7_Ej_Prac/ecommerce/src/app/guards/guard-access-login.guard.ts
+++ PEC7_Ej_Prac/ecommerce/src/app/guards/guard-access-login.guard.ts
@@ -1,5 +1,7 @@
 import { CanActivateFn } from '@angular/router';
 
+/* En este guard lo que hacemos es mirar haber si el usuario esta autentificado o no, es decir cogemos la informacion
+del localStorage y dependiendo si hay informacion o no devolvemos un true o un false */
 export const guardAccessLoginGuard: CanActivateFn = (route, state): any => {
   return localStorage.getItem('user-info') ? false : true;
 };
